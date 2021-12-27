1b5c4dbb058994a6a193e8e9d8f83b2a9039d770
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-12-28 00:28:32 +0700

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   //API_LOCAL
-  API_LOCAL: 'http://localhost:8080/api/',
+  // API_LOCAL: 'http://localhost:8080/api/',
   //API_SERVER
   API_SERVER: 'https://c0821i1-test.herokuapp.com/api/',
   production: true
